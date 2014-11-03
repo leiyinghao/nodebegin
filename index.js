7f4997e83b00bf8a040e604a9df3335753fc1a36
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-11-03 23:23:57 +0800

    程序响应请求处理

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var server = require("./server2")
-server.start();
+var server = require("./server2");
+var router = require("./router");
+var requestHandlers = require('./requestHandlers');
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route,handle);
--- server2.js
+++ server2.js
@@ -1,16 +1,22 @@
 var http = require("http")
-var url = request("url")
+var url = require("url")
 
-function start(){
+function start(route,handle){
 
-function onRequest(request,response){
+  function onRequest(request,response){
   var pathname = url.parse(request.url).pathname;
-  console.log("Request path for" + pathname  +"received" );
+  console.log("Request path for" + pathname  +"received and handle is :" + handle );
+
+  //route(handle,pathname);
   response.writeHead(200,{"Content-Type":"text/plain"});
-  response.write("Hello world");
+  var content =  route(handle,pathname);
+  console.log(content);
+
+  response.write(content);
+ //response.write("Hello world");
   response.end();
 
-}
+  }
   http.createServer(onRequest).listen(8888);
   console.log("server has stated  8888");
 }
