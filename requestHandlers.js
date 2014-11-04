4eb277594a98c2c3dbea55d2cac5e35f956bd1e0
Yinghao Lei <leiyinghao.bj@gmail.com>
2014-11-04 19:17:53 +0800

    增加非阻塞操作ecec()

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,18 @@
+var exec = require("child_process").exec;
+
 function start(){
 	console.log("Request handler 'start' was called");
+	var content = "emputy";
+	/*
 	function sleep(milliSeconds){
 	  var starTime = new Date().getTime();
 	  while (new Date().getTime() < starTime + milliSeconds);
         }
 
 	sleep(10000);
-        
-	return "Hello Start";
+        */
+	//return "Hello Start";
+	return content;
   }
 
 function upload(){
